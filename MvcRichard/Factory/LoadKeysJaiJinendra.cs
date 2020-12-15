708135edff273c7943c964e7bfcb28ec26bd5eea
richardpfletcher <richardpfletcher@gmail.com>
2020-12-15 07:51:01 -0600

    latest

--- MvcRichard/Factory/LoadKeysJaiJinendra.cs
+++ MvcRichard/Factory/LoadKeysJaiJinendra.cs
@@ -16,21 +16,108 @@ namespace MvcRichard.Factory
             //talks
 
             list.Add(new BookModel(counter++, "Intro"));
-            list.Add(new BookModel(counter++, "Message1"));
-            list.Add(new BookModel(counter++, "Message2"));
-            list.Add(new BookModel(counter++, "Message3"));
-            list.Add(new BookModel(counter++, "Message4"));
-            list.Add(new BookModel(counter++, "Message5"));
-            list.Add(new BookModel(counter++, "Message6"));
-            list.Add(new BookModel(counter++, "Message7"));
-            list.Add(new BookModel(counter++, "Message8"));
-            list.Add(new BookModel(counter++, "Message9)"));
-            list.Add(new BookModel(counter++, "Message10"));
-            list.Add(new BookModel(counter++, "Message11"));
-            list.Add(new BookModel(counter++, "Message12"));
-            list.Add(new BookModel(counter++, "Message13"));
-            list.Add(new BookModel(counter++, "Message14"));
-            list.Add(new BookModel(counter++, "Message15"));
+            list.Add(new BookModel(counter++, "Intro Take Two"));
+            
+            list.Add(new BookModel(counter++, "Jainism Mahavira quotes"));
+            list.Add(new BookModel(counter++, "Fourteen Video Game Stages Of Spiritual Development"));
+            list.Add(new BookModel(counter++, "Video game of life"));
+            list.Add(new BookModel(counter++, "Jain ethics and five vows"));
+            list.Add(new BookModel(counter++, "Sat Chit Ananda"));
+            list.Add(new BookModel(counter++, "Violence in our leaders"));
+            list.Add(new BookModel(counter++, "Did Jainism Help Shape the American Civil Rights Movement"));
+            list.Add(new BookModel(counter++, "My Trip to the Land of Gandhi"));
+            list.Add(new BookModel(counter++, "My Trip to the Land of Gandhi 2"));
+            list.Add(new BookModel(counter++, "Non-violence in protests"));
+            list.Add(new BookModel(counter++, "Non-violence in schools"));
+            list.Add(new BookModel(counter++, "Non-violence in relationships"));
+            list.Add(new BookModel(counter++, "Non-violence in your mind and body"));
+            list.Add(new BookModel(counter++, "Emotions"));
+            list.Add(new BookModel(counter++, "New Thought"));
+            list.Add(new BookModel(counter++, "New Concepts"));
+            list.Add(new BookModel(counter++, "New Wiring"));
+            list.Add(new BookModel(counter++, "New Personality"));
+            list.Add(new BookModel(counter++, "New Human"));
+            list.Add(new BookModel(counter++, "You Are Closer Than You Think"));
+            list.Add(new BookModel(counter++, "Non-violence in media"));
+            list.Add(new BookModel(counter++, "The Drama Queen"));
+            list.Add(new BookModel(counter++, "Non-violence in Politics"));
+            list.Add(new BookModel(counter++, "Non-violence in economy"));
+            list.Add(new BookModel(counter++, "Non-violence in personal beliefs"));
+            list.Add(new BookModel(counter++, "Non-violence in diet"));
+
+            list.Add(new BookModel(counter++, "Yamas list of do’s"));
+            list.Add(new BookModel(counter++, "Yamas-Sat Chit Ananda"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-Your body Is Your Drug Store"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-Playing With Your Chemistry Kit"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-What Is Panpsychism"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-Mind and Body"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-Emotions"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-New Thought"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-New Concepts"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-New Wiring"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-New Personalit"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-New Human"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-You Are Closer Than You Think"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-Where Do These Memories Come From"));
+            list.Add(new BookModel(counter++, "Shaucha(शौच) purity-Mindfulness"));
+            list.Add(new BookModel(counter++, "Āsana Postures"));
+            list.Add(new BookModel(counter++, "Āsana Postures-Temple Of God"));
+            list.Add(new BookModel(counter++, "Āsana Postures-Chakras"));
+            list.Add(new BookModel(counter++, "Āsana Postures-DNA"));
+            list.Add(new BookModel(counter++, "Āsana Postures-Bruce Lipton"));
+            list.Add(new BookModel(counter++, "Āsana Postures-Endocrine System"));
+            list.Add(new BookModel(counter++, "Prānāyāma"));
+            list.Add(new BookModel(counter++, "Prānāyāma-The Breath"));
+            list.Add(new BookModel(counter++, "Prānāyāma-Breathing Through Your Mouth"));
+            list.Add(new BookModel(counter++, "Prānāyāma-Fine Tune Your Radio Station"));
+            list.Add(new BookModel(counter++, "Pratyāhāra withdrawing of the external senses"));
+            list.Add(new BookModel(counter++, "Pratyāhāra-Five Internal Senses"));
+            list.Add(new BookModel(counter++, "Dhāraṇā Fixity--Breathe"));
+            list.Add(new BookModel(counter++, "Dhāraṇā Fixity-Fixity"));
+            list.Add(new BookModel(counter++, "Dhāraṇā Fixity--Recalibrate"));
+            list.Add(new BookModel(counter++, "Dhyāna Meditation"));
+            list.Add(new BookModel(counter++, "Dhyana Meditation-Simple Meditation"));
+            list.Add(new BookModel(counter++, "Dhyana Meditation-Anima"));
+            list.Add(new BookModel(counter++, "Dhyana Meditation-Where Would I Be Without Meditation"));
+            list.Add(new BookModel(counter++, "Dhyana Meditation-Mediation"));
+            list.Add(new BookModel(counter++, "Dhyana Meditation-Carry Your Meditation Into Your Daily Life"));
+            list.Add(new BookModel(counter++, "Dhyana Meditation-The Word"));
+            list.Add(new BookModel(counter++, "Samādhi समाधि"));
+            list.Add(new BookModel(counter++, "Jain Meditation Old School"));
+            list.Add(new BookModel(counter++, "Preksha Meditation"));
+            list.Add(new BookModel(counter++, "Perception Of Breathing"));
+            list.Add(new BookModel(counter++, "Perception Of Alternate Breathing"));
+            list.Add(new BookModel(counter++, "Perception Of Alternate Breathing Exercise"));
+            list.Add(new BookModel(counter++, "The Kayotsarga"));
+            list.Add(new BookModel(counter++, "Pindāstha Dhyāna"));
+            list.Add(new BookModel(counter++, "Padāstha Dhyāna"));
+            list.Add(new BookModel(counter++, "Rūpāstha Dhyāna"));
+            list.Add(new BookModel(counter++, "Rūpātita Dhyāna"));
+            list.Add(new BookModel(counter++, "The Spiritual Elevation And Salvation Of The Soul"));
+            list.Add(new BookModel(counter++, "Jain Meditation New School"));
+            list.Add(new BookModel(counter++, "Relaxation"));
+            list.Add(new BookModel(counter++, "Internal Journey"));
+            list.Add(new BookModel(counter++, "Modern day version"));
+            list.Add(new BookModel(counter++, "Modern day version meditation"));
+            list.Add(new BookModel(counter++, "Perception of Breathing New"));
+            list.Add(new BookModel(counter++, "SOHUM"));
+            list.Add(new BookModel(counter++, "Om Meditation"));
+            list.Add(new BookModel(counter++, "Perception of Breathing New"));
+            list.Add(new BookModel(counter++, "How To Do Alternate Nostril Breathing(Nadi Shodhana)"));
+            list.Add(new BookModel(counter++, "The Breath"));
+            list.Add(new BookModel(counter++, "Breathing Through Your Mouth"));
+            list.Add(new BookModel(counter++, "Fine Tune Your Radio Station"));
+            list.Add(new BookModel(counter++, "Monitoring Your Thoughts And Emotions"));
+            list.Add(new BookModel(counter++, "Mind Movies"));
+            list.Add(new BookModel(counter++, "11-28-2020 40 Days 40 Nights"));
+            list.Add(new BookModel(counter++, "Teachers Pramilaji and Priyaji"));
+            list.Add(new BookModel(counter++, "Asanas"));
+
+            
+
+
+
 
 
 
