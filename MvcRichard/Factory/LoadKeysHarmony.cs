d6b799185d15b0b8da5e59e3a21274a589759283
richardpfletcher <richardpfletcher@gmail.com>
2021-01-11 09:57:53 -0600

    LoadkeyHarmony

--- MvcRichard/Factory/LoadKeysHarmony.cs
+++ MvcRichard/Factory/LoadKeysHarmony.cs
@@ -17,22 +17,66 @@ namespace MvcRichard.Factory
 
             list.Add(new BookModel(counter++, "Harmony"));
 
-            list.Add(new BookModel(counter++, "Depression and PTSD"));
-            list.Add(new BookModel(counter++, "Burned Out"));
-            list.Add(new BookModel(counter++, "Intuition And Synchronicity"));
-            list.Add(new BookModel(counter++, "Harmony Health"));
-            list.Add(new BookModel(counter++, "Dialogue Vs Flaming"));
-            list.Add(new BookModel(counter++, "The Quantum Field"));
-            list.Add(new BookModel(counter++, "David The Dragon"));
-            list.Add(new BookModel(counter++, "Chakras"));
-            list.Add(new BookModel(counter++, "A Good Night Sleep"));
-            list.Add(new BookModel(counter++, "Silence"));
-            list.Add(new BookModel(counter++, "Cultivating The Mind"));
-            list.Add(new BookModel(counter++, "Ego Vs Humility"));
-            list.Add(new BookModel(counter++, "The Mind Of God"));
-            list.Add(new BookModel(counter++, "Think Outside Of The Box"));
-            list.Add(new BookModel(counter++, "Closing"));
+            
+            list.Add(new BookModel(counter++, "Three"));
+           
+            list.Add(new BookModel(counter++, "Five"));
+            
+            list.Add(new BookModel(counter++, "Eight"));
+            
+            list.Add(new BookModel(counter++, "Ten"));
+            
+            list.Add(new BookModel(counter++, "Twelve"));
+            
+            list.Add(new BookModel(counter++, "Eighteen"));
+            
 
+            list.Add(new BookModel(counter++, "Twenty-One"));
+            list.Add(new BookModel(counter++, "Twenty-Two"));
+            list.Add(new BookModel(counter++, "Twenty-Three"));
+            
+            list.Add(new BookModel(counter++, "Twenty-Eight"));
+            list.Add(new BookModel(counter++, "Twenty-Nine"));
+
+            list.Add(new BookModel(counter++, "Thirty"));
+            list.Add(new BookModel(counter++, "Thirty-One"));
+           
+            list.Add(new BookModel(counter++, "Thirty-Three"));
+           
+            list.Add(new BookModel(counter++, "Thirty-Six"));
+            
+            list.Add(new BookModel(counter++, "Thirty-Eight"));
+            list.Add(new BookModel(counter++, "Thirty-Nine"));
+
+            
+            list.Add(new BookModel(counter++, "Forty-Two"));
+           
+            list.Add(new BookModel(counter++, "Forty-Six"));
+            
+            list.Add(new BookModel(counter++, "Fifty-Five"));
+           
+            list.Add(new BookModel(counter++, "Fifty-Eight"));
+            
+
+            list.Add(new BookModel(counter++, "Sixty"));
+            list.Add(new BookModel(counter++, "Sixty-One"));
+            list.Add(new BookModel(counter++, "Sixty-Two"));
+            
+            list.Add(new BookModel(counter++, "Sixty-Five"));
+            list.Add(new BookModel(counter++, "Sixty-Six"));
+            list.Add(new BookModel(counter++, "Sixty-Seven"));
+            
+            list.Add(new BookModel(counter++, "Seventy-One"));
+            list.Add(new BookModel(counter++, "Seventy-Two"));
+            list.Add(new BookModel(counter++, "Seventy-Three"));
+            
+            list.Add(new BookModel(counter++, "Seventy-Six"));
+            list.Add(new BookModel(counter++, "Seventy-Seven"));
+            list.Add(new BookModel(counter++, "Seventy-Eight"));
+            list.Add(new BookModel(counter++, "Seventy-Nine"));
+
+            list.Add(new BookModel(counter++, "Eighty"));
+            list.Add(new BookModel(counter++, "Eighty-One"));
 
 
         }
