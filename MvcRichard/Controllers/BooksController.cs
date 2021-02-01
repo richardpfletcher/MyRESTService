c1ecf110c17d8d7123ae59ae1e6046e91118eb5f
richardpfletcher <richardpfletcher@gmail.com>
2021-02-01 03:33:57 -0600

    Created Mystical Adventures project

--- MvcRichard/Controllers/BooksController.cs
+++ MvcRichard/Controllers/BooksController.cs
@@ -1999,6 +1999,49 @@ namespace MvcRichard.Controllers
 
         }
 
+        public ActionResult MysticalAdventures()
+        {
+
+            LoadKeysMysticalAdventures s1 = LoadKeysMysticalAdventures.Instance();
+            List<BookModel> items = LoadKeysMysticalAdventures.list;
+
+            String Path = Server.MapPath("/Audio/Books/MysticalAdventures");
+            String[] FileNames = Directory.GetFiles(Path);
+
+            List<DocumentModel> list = new List<DocumentModel>();
+
+            foreach (var data in items) //iterate the file list
+            {
+                foreach (string path in FileNames) //iterate the file list
+                {
+                    string x = path;
+
+                    // Find the last occurrence of N.
+                    int index1 = x.LastIndexOf('\\');
+                    string fullname = x.Substring(index1 + 1);
+
+                    string shortname = fullname.Substring(0, fullname.Length - 4);
+
+                    if (shortname.ToUpper() == data.Chapter.ToUpper())
+                    {
+                        list.Add(new DocumentModel(fullname, shortname, "\\Audio\\Books\\MysticalAdventures\\" + fullname, "http://www.evolutionrevolutionoflove.com/Audio/Books/MysticalAdventures/" + fullname));
+                        break;
+                    }
+                }
+            }
+
+            //InsertRecords myInsertRecords = new InsertRecords();
+            //myInsertRecords.loadData(list);
+
+
+            ViewData["orderData"] = list;
+
+            return View();
+
+        }
+
+
+
 
 
 
