73d8792fc4f2db4fb1ca3ab33f3e302636ded10c
richardpfletcher <richardpfletcher@gmail.com>
2021-02-14 09:57:46 -0600

    add new album to database

--- MvcRichard/Factory/InsertRecords.cs
+++ MvcRichard/Factory/InsertRecords.cs
@@ -43,12 +43,12 @@ namespace MvcRichard.Factory
                         //4   Books
 
 
-                        cmd.Parameters.Add("@Category", SqlDbType.Int).Value = 4;
+                        cmd.Parameters.Add("@Category", SqlDbType.Int).Value = 1;
 
                         // id
                         //part of album It's a beautiful day in the neighboorhood
                         // change this value each time
-                        cmd.Parameters.Add("@PartOfAlbum", SqlDbType.Int).Value = 170;
+                        cmd.Parameters.Add("@PartOfAlbum", SqlDbType.Int).Value = 171;
 
 
                         con.Open();
