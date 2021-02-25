d4ef6deabdd2318e4bf0d74b63e368f345a5eacc
richardpfletcher <richardpfletcher@gmail.com>
2021-02-25 07:43:26 -0600

    Finished Conscious Economics

--- MvcRichard/Factory/LoadKeysEconomics.cs
+++ MvcRichard/Factory/LoadKeysEconomics.cs
@@ -13,25 +13,39 @@ namespace MvcRichard.Factory
         protected LoadKeysEconomics()
         {
             int counter = 0;
-            //talks
+           
 
             list.Add(new BookModel(counter++, "Intro"));
-            list.Add(new BookModel(counter++, "Message1"));
-            list.Add(new BookModel(counter++, "Message2"));
-            list.Add(new BookModel(counter++, "Message3"));
-            list.Add(new BookModel(counter++, "Message4"));
-            list.Add(new BookModel(counter++, "Message5"));
-            list.Add(new BookModel(counter++, "Message6"));
-            list.Add(new BookModel(counter++, "Message7"));
-            list.Add(new BookModel(counter++, "Message8"));
-            list.Add(new BookModel(counter++, "Message9)"));
-            list.Add(new BookModel(counter++, "Message10"));
-            list.Add(new BookModel(counter++, "Message11"));
-            list.Add(new BookModel(counter++, "Message12"));
-            list.Add(new BookModel(counter++, "Message13"));
-            list.Add(new BookModel(counter++, "Message14"));
-            list.Add(new BookModel(counter++, "Message15"));
-
+            list.Add(new BookModel(counter++, "Mediation"));
+            list.Add(new BookModel(counter++, "Empty Trinkets Of Life"));
+            list.Add(new BookModel(counter++, "Conscious"));
+            list.Add(new BookModel(counter++, "Fourteen Video Game Stages Of Spiritual Development"));
+            list.Add(new BookModel(counter++, "Economics"));
+            list.Add(new BookModel(counter++, "Indigenous Americans Spirituality and Ecos"));
+            list.Add(new BookModel(counter++, "Aboriginal"));
+            list.Add(new BookModel(counter++, "Apathy"));
+            list.Add(new BookModel(counter++, "Chop Down The Trees"));
+            list.Add(new BookModel(counter++, "Gaia -Message5"));
+            list.Add(new BookModel(counter++, "Gaia -Message10"));
+            list.Add(new BookModel(counter++, "Children Working In Mines"));
+            list.Add(new BookModel(counter++, "Global Prosperity For All"));
+            list.Add(new BookModel(counter++, "I Can't See It So It Can't Be Real"));
+            list.Add(new BookModel(counter++, "Magnets Are The Future"));
+            list.Add(new BookModel(counter++, "Man Vision Of Paradise"));
+            list.Add(new BookModel(counter++, "Non -violence in economy"));
+            list.Add(new BookModel(counter++, "Ten Million Years From Now"));
+            list.Add(new BookModel(counter++, "Recalibration"));
+            list.Add(new BookModel(counter++, "Reset Button"));
+            list.Add(new BookModel(counter++, "The Law Of Calibration"));
+            list.Add(new BookModel(counter++, "The Revolution Is Here"));
+            list.Add(new BookModel(counter++, "The Wise Man Just Simply Smiles"));
+            list.Add(new BookModel(counter++, "To Be Spiritual Is To Be Practical"));
+            list.Add(new BookModel(counter++, "We Decide The Outcome"));
+            list.Add(new BookModel(counter++, "Wisdom From Your Ancestors"));
+            list.Add(new BookModel(counter++, "You Are Closer Than You Think"));
+            list.Add(new BookModel(counter++, "3/16/2018 Panpsychism"));
+            list.Add(new BookModel(counter++, "4/28/2018 Chicken"));
+            list.Add(new BookModel(counter++, "Closing"));
 
 
         }
