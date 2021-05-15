5ba739aba39ae7269e7d51a835dc860ba37e0a05
richardpfletcher <richardpfletcher@gmail.com>
2021-05-15 07:32:44 -0500

    created Recalibration project

--- MvcRichard/Controllers/BooksController.cs
+++ MvcRichard/Controllers/BooksController.cs
@@ -2165,6 +2165,47 @@ namespace MvcRichard.Controllers
 
         }
 
+        public ActionResult Recalibration()
+        {
+
+            LoadKeysRecalibration s1 = LoadKeysRecalibration.Instance();
+            List<BookModel> items = LoadKeysRecalibration.list;
+
+            String Path = Server.MapPath("/Audio/Books/Recalibration");
+            String[] FileNames = Directory.GetFiles(Path);
+
+            List<DocumentModel> list = new List<DocumentModel>();
+
+            foreach (var data in items) //iterate the file list
+            {
+                foreach (string path in FileNames) //iterate the file list
+                {
+                    string x = path;
+
+                    // Find the last occurrence of N.
+                    int index1 = x.LastIndexOf('\\');
+                    string fullname = x.Substring(index1 + 1);
+
+                    string shortname = fullname.Substring(0, fullname.Length - 4);
+
+                    if (shortname.ToUpper() == data.Chapter.ToUpper())
+                    {
+                        list.Add(new DocumentModel(fullname, shortname, "\\Audio\\Books\\Recalibration\\" + fullname, "http://www.evolutionrevolutionoflove.com/Audio/Books/Recalibration/" + fullname));
+                        break;
+                    }
+                }
+            }
+
+            //InsertRecords myInsertRecords = new InsertRecords();
+            //myInsertRecords.loadData(list);
+
+
+            ViewData["orderData"] = list;
+
+            return View();
+
+        }
+
 
 
 
---
+++ MvcRichard/Factory/LoadKeysRecalibration.cs
@@ -0,0 +1,35 @@
+﻿using MvcRichard.Models;
+using System.Collections.Generic;
+
+namespace MvcRichard.Factory
+{
+    internal class LoadKeysRecalibration
+    {
+        private static LoadKeysRecalibration _instance;
+
+        public static List<BookModel> list = new List<BookModel>();
+
+        // Constructor is 'protected'
+        protected LoadKeysRecalibration()
+        {
+            int counter = 0;
+            //talks
+
+            list.Add(new BookModel(counter++, "Intro"));
+            
+
+        }
+
+        public static LoadKeysRecalibration Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new LoadKeysRecalibration();
+            }
+
+            return _instance;
+        }
+    }
+}
