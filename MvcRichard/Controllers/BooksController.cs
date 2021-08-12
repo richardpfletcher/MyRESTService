81ff319977253c08828c339a4e56b53690035665
richardpfletcher <richardpfletcher@gmail.com>
2021-08-12 07:29:24 -0500

    NaropaCommentary

--- MvcRichard/Controllers/BooksController.cs
+++ MvcRichard/Controllers/BooksController.cs
@@ -2374,6 +2374,47 @@ namespace MvcRichard.Controllers
 
         }
 
+        public ActionResult NaropaCommentary()
+        {
+
+            LoadKeysNaropaCommentary s1 = LoadKeysNaropaCommentary.Instance();
+            List<BookModel> items = LoadKeysNaropaCommentary.list;
+
+            String Path = Server.MapPath("/Audio/Books/NaropaCommentary");
+            String[] FileNames = Directory.GetFiles(Path);
+
+            List<DocumentModel> list = new List<DocumentModel>();
+
+            foreach (var data in items) //iterate the file list
+            {
+                foreach (string path in FileNames) //iterate the file list
+                {
+                    string x = path;
+
+                    // Find the last occurrence of N.
+                    int index1 = x.LastIndexOf('\\');
+                    string fullname = x.Substring(index1 + 1);
+
+                    string shortname = fullname.Substring(0, fullname.Length - 4);
+
+                    if (shortname.ToUpper() == data.Chapter.ToUpper())
+                    {
+                        list.Add(new DocumentModel(fullname, shortname, "\\Audio\\Books\\NaropaCommentary\\" + fullname, "http://www.evolutionrevolutionoflove.com/Audio/Books/NaropaCommentary/" + fullname));
+                        break;
+                    }
+                }
+            }
+
+            //InsertRecords myInsertRecords = new InsertRecords();
+            //myInsertRecords.loadData(list);
+
+
+            ViewData["orderData"] = list;
+
+            return View();
+
+        }
+
 
         public ActionResult Definition()
         {
