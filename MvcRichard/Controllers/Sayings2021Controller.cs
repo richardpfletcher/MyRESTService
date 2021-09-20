ffe275b88c22afc63b0e5d565ff3741b165eaec1
richardpfletcher <richardpfletcher@gmail.com>
2021-09-20 17:28:17 -0500

    Grace

--- MvcRichard/Controllers/Sayings2021Controller.cs
+++ MvcRichard/Controllers/Sayings2021Controller.cs
@@ -43,5 +43,77 @@ namespace MvcRichard.Controllers
             return View();
         }
 
+        public ActionResult Sayings28()
+        {
+            String Path = Server.MapPath("/Audio/Sayings/Sayings28");
+            String[] FileNames = Directory.GetFiles(Path);
+
+            List<DocumentModel> list = new List<DocumentModel>();
+
+            foreach (string path in FileNames) //iterate the file list
+            {
+                string x = path;
+
+                // Find the last occurrence of \.
+                int index1 = x.LastIndexOf('\\');
+                string fullname = x.Substring(index1 + 1);
+
+                string shortname = fullname.Substring(0, fullname.Length - 4);
+                if (shortname != "Intro" && shortname.ToUpper() != "ALBUM" && shortname.ToUpper() != "ORGINAL")
+                {
+                    list.Add(new DocumentModel(fullname, shortname, "\\Audio\\Sayings\\Sayings28\\" + fullname, "http://www.evolutionrevolutionoflove.com/Audio/Sayings/Sayings28/" + fullname));
+                }
+            }
+
+
+            //InsertRecords myInsertRecords = new InsertRecords();
+            //myInsertRecords.loadData(list);
+
+
+
+            ViewData["orderData"] = list;
+
+            return View();
+        }
+
+        public ActionResult Sayings29()
+        {
+            String Path = Server.MapPath("/Audio/Sayings/Sayings29");
+            String[] FileNames = Directory.GetFiles(Path);
+
+            List<DocumentModel> list = new List<DocumentModel>();
+
+            foreach (string path in FileNames) //iterate the file list
+            {
+                string x = path;
+
+                // Find the last occurrence of \.
+                int index1 = x.LastIndexOf('\\');
+                string fullname = x.Substring(index1 + 1);
+
+                string shortname = fullname.Substring(0, fullname.Length - 4);
+                if (shortname != "Intro" && shortname.ToUpper() != "ALBUM" && shortname.ToUpper() != "ORGINAL")
+                {
+                    list.Add(new DocumentModel(fullname, shortname, "\\Audio\\Sayings\\Sayings29\\" + fullname, "http://www.evolutionrevolutionoflove.com/Audio/Sayings/Sayings29/" + fullname));
+                }
+            }
+
+
+            //InsertRecords myInsertRecords = new InsertRecords();
+            //myInsertRecords.loadData(list);
+
+
+
+            ViewData["orderData"] = list;
+
+            return View();
+        }
+
+
+
+
+
+
+
     }
 }
