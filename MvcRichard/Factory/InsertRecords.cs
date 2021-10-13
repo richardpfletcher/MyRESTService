ea36fe96d431de1223e2910d3f7d2c8e5c580439
richardpfletcher <richardpfletcher@gmail.com>
2021-10-13 10:49:50 -0500

    Create maya cd

--- MvcRichard/Factory/InsertRecords.cs
+++ MvcRichard/Factory/InsertRecords.cs
@@ -48,7 +48,7 @@ namespace MvcRichard.Factory
                         // id
                         //part of album It's a beautiful day in the neighboorhood
                         // change this value each time
-                        cmd.Parameters.Add("@PartOfAlbum", SqlDbType.Int).Value = 203;
+                        cmd.Parameters.Add("@PartOfAlbum", SqlDbType.Int).Value = 204;
 
 
                         con.Open();
