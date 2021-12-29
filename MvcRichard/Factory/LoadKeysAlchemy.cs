6fe46f522f73f01f82956631be2b167f58b5372d
richardpfletcher <richardpfletcher@gmail.com>
2021-12-29 09:58:33 -0600

    Checking in Alchemy and dragons Naropa 2

--- MvcRichard/Factory/LoadKeysAlchemy.cs
+++ MvcRichard/Factory/LoadKeysAlchemy.cs
@@ -16,20 +16,49 @@ namespace MvcRichard.Factory
             //talks
 
             list.Add(new BookModel(counter++, "Intro"));
-            list.Add(new BookModel(counter++, "Depression and PTSD"));
-            list.Add(new BookModel(counter++, "Burned Out"));
-            list.Add(new BookModel(counter++, "Intuition And Synchronicity"));
-            list.Add(new BookModel(counter++, "Cloudy Health"));
-            list.Add(new BookModel(counter++, "Dialogue Vs Flaming"));
-            list.Add(new BookModel(counter++, "The Quantum Field"));
-            list.Add(new BookModel(counter++, "David The Dragon"));
-            list.Add(new BookModel(counter++, "Chakras"));
-            list.Add(new BookModel(counter++, "A Good Night Sleep"));
-            list.Add(new BookModel(counter++, "Silence"));
-            list.Add(new BookModel(counter++, "Cultivating The Mind"));
-            list.Add(new BookModel(counter++, "Ego Vs Humility"));
-            list.Add(new BookModel(counter++, "The Mind Of God"));
-            list.Add(new BookModel(counter++, "Think Outside Of The Box"));
+
+            list.Add(new BookModel(counter++, "What Does Alchemy Have To Do With Me"));
+            list.Add(new BookModel(counter++, "Fourteen Video Game Stages Of Spiritual Development"));
+            list.Add(new BookModel(counter++, "Video game of life"));
+            list.Add(new BookModel(counter++, "Fine Tune Your Radio Station"));
+            list.Add(new BookModel(counter++, "Synchronicity"));
+            list.Add(new BookModel(counter++, "Michael Singer"));
+            list.Add(new BookModel(counter++, "What is “The Surrender Experiment"));
+            list.Add(new BookModel(counter++, "The Untethered Soul"));
+            list.Add(new BookModel(counter++, "Alchemy At Its Finest"));
+            list.Add(new BookModel(counter++, "Let go of your aggravations"));
+            list.Add(new BookModel(counter++, "Let go"));
+            list.Add(new BookModel(counter++, "Let it go"));
+            list.Add(new BookModel(counter++, "It takes two to tango"));
+            list.Add(new BookModel(counter++, "Relax and Release"));
+            list.Add(new BookModel(counter++, "Relax"));
+            list.Add(new BookModel(counter++, "How to learn to live with the world around you"));
+            list.Add(new BookModel(counter++, "Relax Your Muscles Every Day"));
+            list.Add(new BookModel(counter++, "Control freak"));
+            list.Add(new BookModel(counter++, "Risking failure"));
+            list.Add(new BookModel(counter++, "Recognize I have a problem and it is me"));
+            list.Add(new BookModel(counter++, "The more control you have the more out of control you are"));
+            list.Add(new BookModel(counter++, "Is it fixable"));
+            list.Add(new BookModel(counter++, "Before you judge others"));
+            list.Add(new BookModel(counter++, "Recognition of your problem"));
+            list.Add(new BookModel(counter++, "Everything causes a disturbance"));
+            list.Add(new BookModel(counter++, "Why are you disturbed by the weather"));
+            list.Add(new BookModel(counter++, "What part of you is disturbed"));
+            list.Add(new BookModel(counter++, "We protect ourselves from what we feel uncomfortable inside of us"));
+            list.Add(new BookModel(counter++, "The highest way to let go of a disturbance is to relax"));
+            list.Add(new BookModel(counter++, "Don’t complain just relax"));
+            list.Add(new BookModel(counter++, "Discipline leads to surrender"));
+            list.Add(new BookModel(counter++, "When you relax you disengage with the disturbance"));
+            list.Add(new BookModel(counter++, "All Things Must Pass"));
+            list.Add(new BookModel(counter++, "Water underneath the bridge"));
+            list.Add(new BookModel(counter++, "Using the world to define you"));
+            list.Add(new BookModel(counter++, "Road Rage"));
+            list.Add(new BookModel(counter++, "Suppression pushes the event away"));
+            list.Add(new BookModel(counter++, "No experience is traumatic it is how we handle it"));
+            list.Add(new BookModel(counter++, "The same event occurs in the family"));
+            list.Add(new BookModel(counter++, "The mosquito itch"));
+            list.Add(new BookModel(counter++, "That nagging feeling"));
+            list.Add(new BookModel(counter++, "Laugh At Life"));
             list.Add(new BookModel(counter++, "Closing"));
 
 
--- MvcRichard/Factory/LoadKeysDragonsNaropa2.cs
+++ MvcRichard/Factory/LoadKeysDragonsNaropa2.cs
@@ -17,7 +17,47 @@ namespace MvcRichard.Factory
             //talks
 
             list.Add(new BookModel(counter++, "Intro"));
-            
+
+            list.Add(new BookModel(counter++, "Little Ricky’s History Of The Dragons"));
+            list.Add(new BookModel(counter++, "Initiation"));
+            list.Add(new BookModel(counter++, "Mediation Ganges"));
+            list.Add(new BookModel(counter++, "The palace in the sky"));
+            list.Add(new BookModel(counter++, "Brahmanand-Palace in the sky"));
+            list.Add(new BookModel(counter++, "Tulsidas-Glimpse Of The Invisible"));
+            list.Add(new BookModel(counter++, "Brahmanand-the miracle"));
+            list.Add(new BookModel(counter++, "Monroe Institute"));
+            list.Add(new BookModel(counter++, "Monroe Experience Part 1"));
+            list.Add(new BookModel(counter++, "Monroe Experience Part 2"));
+            list.Add(new BookModel(counter++, "Monroe Adventure 1985 part 1"));
+            list.Add(new BookModel(counter++, "Monroe Adventure 1985 part 2"));
+            list.Add(new BookModel(counter++, "Monroe Adventure 1985 part 3"));
+            list.Add(new BookModel(counter++, "Kabbalah"));
+            list.Add(new BookModel(counter++, "Council Meetings"));
+            list.Add(new BookModel(counter++, "Gifts Of The Spirit"));
+            list.Add(new BookModel(counter++, "From Me To We"));
+            list.Add(new BookModel(counter++, "Dragon Tales"));
+            list.Add(new BookModel(counter++, "Dragon Tales 2"));
+            list.Add(new BookModel(counter++, "Dragon Tales Commentary"));
+            list.Add(new BookModel(counter++, "The Role of Dragons in Tibetan Culture and Faith"));
+            list.Add(new BookModel(counter++, "Teachings Of The Dragons"));
+            list.Add(new BookModel(counter++, "We all have friends that are with us"));
+            list.Add(new BookModel(counter++, "Palden Lhamo"));
+            list.Add(new BookModel(counter++, "Sri Ramakrishna on the Divine Mother"));
+            list.Add(new BookModel(counter++, "What is empowerment"));
+            list.Add(new BookModel(counter++, "Palden Lhamo Empowerment"));
+            list.Add(new BookModel(counter++, "Tummo"));
+            list.Add(new BookModel(counter++, "Tummo Commentary"));
+            list.Add(new BookModel(counter++, "Phowa"));
+            list.Add(new BookModel(counter++, "The 8 Stages of Dissolution"));
+            list.Add(new BookModel(counter++, "Phowa Commentary"));
+            list.Add(new BookModel(counter++, "Illusory Body"));
+            list.Add(new BookModel(counter++, "Your body Is Your Drug Store"));
+            list.Add(new BookModel(counter++, "Clear Light"));
+            list.Add(new BookModel(counter++, "Dream Yoga"));
+            list.Add(new BookModel(counter++, "Bardo Yoga"));
+
+
+
 
 
 
