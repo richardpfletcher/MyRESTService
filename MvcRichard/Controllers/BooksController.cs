2e04649aef7c930471e31b95a906d132efff1926
richardpfletcher <richardpfletcher@gmail.com>
2023-01-04 07:18:53 -0600

    created central sun

--- MvcRichard/Controllers/BooksController.cs
+++ MvcRichard/Controllers/BooksController.cs
@@ -3451,5 +3451,47 @@ namespace MvcRichard.Controllers
             return View();
         }
 
+        public ActionResult CentralSun()
+        {
+
+
+            LoadKeysCentralSun s1 = LoadKeysCentralSun.Instance();
+            List<BookModel> items = LoadKeysCentralSun.list;
+
+            String Path = Server.MapPath("/Audio/Books/CentralSun");
+            String[] FileNames = Directory.GetFiles(Path);
+
+            List<DocumentModel> list = new List<DocumentModel>();
+
+            foreach (var data in items) //iterate the file list
+            {
+                foreach (string path in FileNames) //iterate the file list
+                {
+                    string x = path;
+
+                    // Find the last occurrence of N.
+                    int index1 = x.LastIndexOf('\\');
+                    string fullname = x.Substring(index1 + 1);
+
+                    string shortname = fullname.Substring(0, fullname.Length - 4);
+
+                    if (shortname.ToUpper() == data.Chapter.ToUpper())
+                    {
+                        list.Add(new DocumentModel(fullname, shortname, "\\Audio\\Books\\CentralSun\\" + fullname, "http://www.evolutionrevolutionoflove.com/Audio/Books/CentralSun/" + fullname));
+                        break;
+                    }
+                }
+            }
+
+            //InsertRecords myInsertRecords = new InsertRecords();
+            //myInsertRecords.loadData(list);
+
+
+            ViewData["orderData"] = list;
+
+            return View();
+        }
+
+
     }
 }
