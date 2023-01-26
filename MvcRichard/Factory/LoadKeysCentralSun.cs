55d0b9be407f18d3dfa61adbe9ac309d8805ef01
richardpfletcher <richardpfletcher@gmail.com>
2023-01-26 06:10:54 -0600

    check in central sun

--- MvcRichard/Factory/LoadKeysCentralSun.cs
+++ MvcRichard/Factory/LoadKeysCentralSun.cs
@@ -16,23 +16,23 @@ namespace MvcRichard.Factory
             //talks
 
             list.Add(new BookModel(counter++, "Intro"));
-            list.Add(new BookModel(counter++, "Message1"));
-            list.Add(new BookModel(counter++, "Message2"));
-            list.Add(new BookModel(counter++, "Message3"));
-            list.Add(new BookModel(counter++, "Message4"));
-            list.Add(new BookModel(counter++, "Message5"));
-            list.Add(new BookModel(counter++, "Message6"));
-            list.Add(new BookModel(counter++, "Message7"));
-            list.Add(new BookModel(counter++, "Message8"));
-            list.Add(new BookModel(counter++, "Message9)"));
-            list.Add(new BookModel(counter++, "Message10"));
-            list.Add(new BookModel(counter++, "Message11"));
-            list.Add(new BookModel(counter++, "Message12"));
-            list.Add(new BookModel(counter++, "Message13"));
-            list.Add(new BookModel(counter++, "Message14"));
-            list.Add(new BookModel(counter++, "Message15"));
 
-
+            list.Add(new BookModel(counter++, "Sine waves"));
+            list.Add(new BookModel(counter++, "Meditate constantly"));
+            list.Add(new BookModel(counter++, "How Can a Fish Drown In Water"));
+            list.Add(new BookModel(counter++, "God is a cosmic surfer"));
+            list.Add(new BookModel(counter++, "Sri Yukteswar Intro Holy Science"));
+            list.Add(new BookModel(counter++, "Sri Yukteswar Reasoning Behind The Cycles of Time"));
+            list.Add(new BookModel(counter++, "The Yuga Cycles & Yogic "));
+            list.Add(new BookModel(counter++, "The Science of Sirius Mythology"));
+            list.Add(new BookModel(counter++, "Binary Stars"));
+            list.Add(new BookModel(counter++, "The Great Central Sun"));
+            list.Add(new BookModel(counter++, "The central sun"));
+            list.Add(new BookModel(counter++, "Darkness Before Dawn"));
+            list.Add(new BookModel(counter++, "Think Outside Of The Box"));
+            list.Add(new BookModel(counter++, "Take off your mask"));
+            list.Add(new BookModel(counter++, "We See Only 1 % Of The Light Spectrum"));
+            list.Add(new BookModel(counter++, "Closing"));
 
         }
 
