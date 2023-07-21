957846edb54270b6479689b76ce3cd04ae73833f
richardpfletcher <richardpfletcher@gmail.com>
2023-07-21 07:24:27 -0500

    checking in madison

--- MvcRichard/Controllers/BooksController.cs
+++ MvcRichard/Controllers/BooksController.cs
@@ -4068,5 +4068,46 @@ public ActionResult Hacks()
             return View();
         }
 
+        public ActionResult MadisonAvenue()
+        {
+
+
+            LoadKeysMadisonAvenue s1 = LoadKeysMadisonAvenue.Instance();
+            List<BookModel> items = LoadKeysMadisonAvenue.list;
+
+            String Path = Server.MapPath("/Audio/Books/MadisonAvenue");
+            String[] FileNames = Directory.GetFiles(Path);
+
+            List<DocumentModel> list = new List<DocumentModel>();
+
+            foreach (var data in items) //iterate the file list
+            {
+                foreach (string path in FileNames) //iterate the file list
+                {
+                    string x = path;
+
+                    // Find the last occurrence of N.
+                    int index1 = x.LastIndexOf('\\');
+                    string fullname = x.Substring(index1 + 1);
+
+                    string shortname = fullname.Substring(0, fullname.Length - 4);
+
+                    if (shortname.ToUpper() == data.Chapter.ToUpper())
+                    {
+                        list.Add(new DocumentModel(fullname, shortname, "\\Audio\\Books\\MadisonAvenue\\" + fullname, "http://www.evolutionrevolutionoflove.com/Audio/Books/MadisonAvenue/" + fullname));
+                        break;
+                    }
+                }
+            }
+
+            //InsertRecords myInsertRecords = new InsertRecords();
+            //myInsertRecords.loadData(list);
+
+
+            ViewData["orderData"] = list;
+
+            return View();
+        }
+
     }
 }
---
+++ MvcRichard/Factory/LoadKeysMadisonAvenue.cs
@@ -0,0 +1,36 @@
+﻿using MvcRichard.Models;
+using System.Collections.Generic;
+
+namespace MvcRichard.Factory
+{
+    internal class LoadKeysMadisonAvenue
+    {
+        private static LoadKeysMadisonAvenue _instance;
+
+        public static List<BookModel> list = new List<BookModel>();
+
+        // Constructor is 'protected'
+        protected LoadKeysMadisonAvenue()
+        {
+            int counter = 0;
+            //talks
+
+            list.Add(new BookModel(counter++, "Intro"));
+           
+
+
+        }
+
+        public static LoadKeysMadisonAvenue Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new LoadKeysMadisonAvenue();
+            }
+
+            return _instance;
+        }
+    }
+}
