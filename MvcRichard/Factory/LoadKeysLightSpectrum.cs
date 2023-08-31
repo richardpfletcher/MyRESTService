7d8f81fd7763ceb880b885f85ba86a42f2e1329b
richardpfletcher <richardpfletcher@gmail.com>
2023-08-31 04:37:06 -0500

    Checking in Light Sprectrum

---
+++ MvcRichard/Factory/LoadKeysLightSpectrum.cs
@@ -0,0 +1,121 @@
+﻿using MvcRichard.Models;
+using System.Collections.Generic;
+
+namespace MvcRichard.Factory
+{
+    internal class LoadKeysLightSpectrum
+    {
+        private static LoadKeysLightSpectrum _instance;
+
+        public static List<BookModel> list = new List<BookModel>();
+
+        // Constructor is 'protected'
+        protected LoadKeysLightSpectrum()
+        {
+            int counter = 0;
+            //talks
+
+            list.Add(new BookModel(counter++, "Intro"));
+
+            list.Add(new BookModel(counter++, "We see only 1 % of the light spectrum"));
+
+
+            list.Add(new BookModel(counter++, "How do geese fly in perfect formation in the sky"));
+            list.Add(new BookModel(counter++, "Some other animals that fly in formation"));
+            list.Add(new BookModel(counter++, "How do birds know when to change direction"));
+            list.Add(new BookModel(counter++, "What is the correlation between geese flying in formation and modern day aircraft"));
+            list.Add(new BookModel(counter++, "Whales can communicate halfway around the world"));
+            list.Add(new BookModel(counter++, "How do whales use echolocation"));
+            list.Add(new BookModel(counter++, "What is the difference between echolocation and sonar"));
+            list.Add(new BookModel(counter++, "Can echolocation be used by humans"));
+            list.Add(new BookModel(counter++, "How can I learn human echolocation"));
+            list.Add(new BookModel(counter++, "What are some common mistakes when learning human echolocation"));
+            list.Add(new BookModel(counter++, "Can I use echolocation while walking or running"));
+            list.Add(new BookModel(counter++, "What are some advanced echolocation exercises"));
+            list.Add(new BookModel(counter++, "Can I use echolocation while driving or cycling"));
+            list.Add(new BookModel(counter++, "Laser Scans Reveal Maya Megalopolis"));
+            list.Add(new BookModel(counter++, "What is the Maya Biosphere Reserve"));
+            list.Add(new BookModel(counter++, "Understandif of history will change tremendously"));
+            list.Add(new BookModel(counter++, "How do schools of fish swim in perfect formation"));
+            list.Add(new BookModel(counter++, "What are some other examples of collective behavior in animals"));
+            list.Add(new BookModel(counter++, "How can animals sense a wildfire"));
+            list.Add(new BookModel(counter++, "What are some animals that can survive a wildfire"));
+            list.Add(new BookModel(counter++, "What was the perfect storm of factors for the destruction of Lahaina"));
+            list.Add(new BookModel(counter++, "Could the fire in Lahaina be prevented"));
+            list.Add(new BookModel(counter++, "What are some other examples of wildfires caused by power lines"));
+            list.Add(new BookModel(counter++, "What are some ways to reduce the risk of wildfires caused by power lines"));
+            list.Add(new BookModel(counter++, "What light spectrum is the divine light inside of us"));
+            list.Add(new BookModel(counter++, "How can I connect with the divine light inside of me"));
+            list.Add(new BookModel(counter++, "Joe Dispensa and the pineal gland"));
+            list.Add(new BookModel(counter++, "What are some other endocrine glands in the brain and what do they do"));
+            list.Add(new BookModel(counter++, "What is the pineal gland and what does it do"));
+            list.Add(new BookModel(counter++, "How can I keep my endocrine system healthy"));
+            list.Add(new BookModel(counter++, "Mystics and neuroscientists talking about same thing pineal gland"));
+            list.Add(new BookModel(counter++, "What are some other spiritual concepts related to the pineal gland"));
+            list.Add(new BookModel(counter++, "Santa Clause and the pineal gland"));
+            list.Add(new BookModel(counter++, "Land of milk and honey and the pineal gland"));
+            list.Add(new BookModel(counter++, "Guru Nanak and nectar"));
+            list.Add(new BookModel(counter++, "What is Ketchi Mudra and Nectar"));
+            list.Add(new BookModel(counter++, "Why don't most people know about this"));
+            list.Add(new BookModel(counter++, "Differences between the Hubble and the James L Web"));
+            list.Add(new BookModel(counter++, "Quantum scientistics and Mystics talking about the same thing"));
+            list.Add(new BookModel(counter++, "Taoist and the endrocrine system"));
+            list.Add(new BookModel(counter++, "Neurscience and chakras"));
+            list.Add(new BookModel(counter++, "What is the difference between chakras and meridians"));
+            list.Add(new BookModel(counter++, "Diseses first appers in the chakras and meridians"));
+            list.Add(new BookModel(counter++, "What is the scientific evidence for chakras and meridians"));
+            list.Add(new BookModel(counter++, "What is the history of chakras and meridians"));
+            list.Add(new BookModel(counter++, "Scientific studies on the effectiveness of chakras and meridians in healing"));
+            list.Add(new BookModel(counter++, "How do people use chakras and meridians in healing practices"));
+            list.Add(new BookModel(counter++, "Western diet is like seeing 1 % of the light spectrum."));
+            list.Add(new BookModel(counter++, "What are some alternatives to the common Western diet"));
+            list.Add(new BookModel(counter++, "How can I change my eating habits to be healthier"));
+            list.Add(new BookModel(counter++, "Due to our lifestyle, we see only 1 % of the life spectrum in life"));
+            list.Add(new BookModel(counter++, "Difference between being custodians of the land or consumers of the land"));
+            list.Add(new BookModel(counter++, "How can we balance being consumers and custodians of the land"));
+            list.Add(new BookModel(counter++, "What are some examples of being custodians of the land"));
+            list.Add(new BookModel(counter++, "Sign posts are all around yet do we have the eyes to see"));
+            list.Add(new BookModel(counter++, "How can man learn to be in harmony with the universe"));
+            list.Add(new BookModel(counter++, "How can I find my true self and purpose"));
+            list.Add(new BookModel(counter++, "What are some examples of personal growth and development"));
+            list.Add(new BookModel(counter++, "How can I set personal growth goals"));
+            list.Add(new BookModel(counter++, "A Spiritual Life It Must Be Boring"));
+            list.Add(new BookModel(counter++, "Kindness Is More Powerful Than Anger"));
+            list.Add(new BookModel(counter++, "Did You Know That The Universe Is Alive"));
+            list.Add(new BookModel(counter++, "Are You Experienced"));
+            list.Add(new BookModel(counter++, "Behold The Palace In The Sky"));
+            list.Add(new BookModel(counter++, "How Can You Use An Old Map In The New World"));
+            list.Add(new BookModel(counter++, "The Diamond Is In The Safe"));
+            list.Add(new BookModel(counter++, "God Talks And Listens"));
+            list.Add(new BookModel(counter++, "The Enemy Lies Within"));
+            list.Add(new BookModel(counter++, "Does God Have A Face"));
+            list.Add(new BookModel(counter++, "Entanglement With God"));
+            list.Add(new BookModel(counter++, "Feet On The Ground Head In Heaven"));
+            list.Add(new BookModel(counter++, "God Is Ever Where But I Can’t See It"));
+            list.Add(new BookModel(counter++, "Is A Spiritual Life Boring"));
+            list.Add(new BookModel(counter++, "Recalibration"));
+            list.Add(new BookModel(counter++, "The Engine Of DNA"));
+            list.Add(new BookModel(counter++, "The Inner Scientist"));
+            list.Add(new BookModel(counter++, "The Signs Of God Are Everywhere"));
+            list.Add(new BookModel(counter++, "We Are Wired For God"));
+            list.Add(new BookModel(counter++, "Are There New Rules To Discover God"));
+            list.Add(new BookModel(counter++, "Kindness Is My Religion"));
+
+
+
+
+        }
+
+        public static LoadKeysLightSpectrum Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new LoadKeysLightSpectrum();
+            }
+
+            return _instance;
+        }
+    }
+}
--- MvcRichard/Factory/LoadKeysMadisonAvenue.cs
+++ MvcRichard/Factory/LoadKeysMadisonAvenue.cs
@@ -16,7 +16,64 @@ namespace MvcRichard.Factory
             //talks
 
             list.Add(new BookModel(counter++, "Intro"));
-           
+
+            list.Add(new BookModel(counter++, "Common values of conscious brands"));
+            list.Add(new BookModel(counter++, "How can we promote these values"));
+            list.Add(new BookModel(counter++, "The history of TV commercials"));
+            list.Add(new BookModel(counter++, "Some pros and cons of drug commercials"));
+            list.Add(new BookModel(counter++, "How did the drug companies take over in the early 1900's"));
+            list.Add(new BookModel(counter++, "Drugs have side effects"));
+            list.Add(new BookModel(counter++, "History of pharmacy before 1900s"));
+            list.Add(new BookModel(counter++, "The top 10 pharmaceutical products"));
+            list.Add(new BookModel(counter++, "Why do drug commercials have people do seem their lives are so happy"));
+            list.Add(new BookModel(counter++, "Some famous drug commercials on TV are"));
+            list.Add(new BookModel(counter++, "Rinvoq can cause side effects"));
+            list.Add(new BookModel(counter++, "Dupixent can cause some side effects"));
+            list.Add(new BookModel(counter++, "Skyrizi can cause some side effects"));
+            list.Add(new BookModel(counter++, "Humira can cause some side effects"));
+            list.Add(new BookModel(counter++, "VRAYLAR can cause some side effects"));
+            list.Add(new BookModel(counter++, "Why do people think I don't have to change my life style taking a drug"));
+            list.Add(new BookModel(counter++, "How to Balance Your Mind, Body, and Soul"));
+            list.Add(new BookModel(counter++, "The United States spends trillions on health care every year"));
+            list.Add(new BookModel(counter++, "Doctors May Need More Education on Nutrition"));
+            list.Add(new BookModel(counter++, "The majority of the US eats junk food"));
+            list.Add(new BookModel(counter++, "How can I reduce my junk food cravings"));
+            list.Add(new BookModel(counter++, "What is the relationship between the microbiome and junk food"));
+            list.Add(new BookModel(counter++, "How do antibotics affect the microbiome"));
+            list.Add(new BookModel(counter++, "Why does junk food taste so good"));
+            list.Add(new BookModel(counter++, "What are some healthy foods that can improve my brain function"));
+            list.Add(new BookModel(counter++, "How much of these foods should I eat"));
+            list.Add(new BookModel(counter++, "The junk food industry spends billions of dollars a year"));
+            list.Add(new BookModel(counter++, "Soft drinks health effects"));
+            list.Add(new BookModel(counter++, "How much sugar is in a can of soda"));
+            list.Add(new BookModel(counter++, "I have friends who only drink soda"));
+            list.Add(new BookModel(counter++, "How to convince someone to drink less soda"));
+            list.Add(new BookModel(counter++, "Sugar content in energy drinks"));
+            list.Add(new BookModel(counter++, "Why does soda industry have professional athletes promote their products"));
+            list.Add(new BookModel(counter++, "How muck money do they make for a commercial on TV"));
+            list.Add(new BookModel(counter++, "If they ban cigarette commercials, why not ban soda commercials"));
+            list.Add(new BookModel(counter++, "Yet the United States is one of the sickest countries in the world"));
+            list.Add(new BookModel(counter++, "United States can make significant lifestyle changes"));
+            list.Add(new BookModel(counter++, "We should have advertising that should only reflect these values"));
+            list.Add(new BookModel(counter++, "I'm saying at times we are sawing off the branch we are sitting on"));
+            list.Add(new BookModel(counter++, "Do you see a time in the advertising industry"));
+            list.Add(new BookModel(counter++, "What do you see for the trends in the next 50 years"));
+            list.Add(new BookModel(counter++, "What are some examples of personalized nutrition"));
+            list.Add(new BookModel(counter++, "What is the difference between personalized nutrition and traditional nutrition"));
+            list.Add(new BookModel(counter++, "Some pros and cons of soda commercials"));
+            list.Add(new BookModel(counter++, "Comparison of soda commercials and drug commercials"));
+            list.Add(new BookModel(counter++, "Effects of commercials on mind body and soul"));
+            list.Add(new BookModel(counter++, "Some examples of harmful commercials are"));
+            list.Add(new BookModel(counter++, "Some examples of good commercials are"));
+            list.Add(new BookModel(counter++, "How commercials affect our emotions"));
+            list.Add(new BookModel(counter++, "Can commercials be addictive"));
+            list.Add(new BookModel(counter++, "Signs of addiction to commercials")); 
+list.Add(new BookModel(counter++, "Quick way to resist the temptation of commercials"));
+            list.Add(new BookModel(counter++, "What are some healthy alternatives to watching commercial"));
+            list.Add(new BookModel(counter++, "How have commercials changed over time"));
+            list.Add(new BookModel(counter++, "Can addiction to commercials be cured completely"));
+            list.Add(new BookModel(counter++, "Closing"));
+
 
 
         }
