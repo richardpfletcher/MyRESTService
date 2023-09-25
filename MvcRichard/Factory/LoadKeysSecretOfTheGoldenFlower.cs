1bb0c2ee1a650784cada3f3c36095c9beb466ea5
richardpfletcher <richardpfletcher@gmail.com>
2023-09-25 05:57:37 -0500

    created secret of the go;lden flower

---
+++ MvcRichard/Factory/LoadKeysSecretOfTheGoldenFlower.cs
@@ -0,0 +1,51 @@
+﻿using MvcRichard.Models;
+using System.Collections.Generic;
+
+namespace MvcRichard.Factory
+{
+    internal class LoadKeysSecretOfTheGoldenFlower
+    {
+        private static LoadKeysSecretOfTheGoldenFlower _instance;
+
+        public static List<BookModel> list = new List<BookModel>();
+
+        // Constructor is 'protected'
+        protected LoadKeysSecretOfTheGoldenFlower()
+        {
+            int counter = 0;
+            //talks
+
+            list.Add(new BookModel(counter++, "Intro"));
+            list.Add(new BookModel(counter++, "Message1"));
+            list.Add(new BookModel(counter++, "Message2"));
+            list.Add(new BookModel(counter++, "Message3"));
+            list.Add(new BookModel(counter++, "Message4"));
+            list.Add(new BookModel(counter++, "Message5"));
+            list.Add(new BookModel(counter++, "Message6"));
+            list.Add(new BookModel(counter++, "Message7"));
+            list.Add(new BookModel(counter++, "Message8"));
+            list.Add(new BookModel(counter++, "Message9)"));
+            list.Add(new BookModel(counter++, "Message10"));
+            list.Add(new BookModel(counter++, "Message11"));
+            list.Add(new BookModel(counter++, "Message12"));
+            list.Add(new BookModel(counter++, "Message13"));
+            list.Add(new BookModel(counter++, "Message14"));
+            list.Add(new BookModel(counter++, "Message15"));
+
+
+
+        }
+
+        public static LoadKeysSecretOfTheGoldenFlower Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new LoadKeysSecretOfTheGoldenFlower();
+            }
+
+            return _instance;
+        }
+    }
+}
