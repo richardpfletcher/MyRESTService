7cb5f263fe52b990d0f8d065bea6832ea94e2c43
richardpfletcher <richardpfletcher@gmail.com>
2024-03-13 05:09:53 -0500

    created little drops of mercy

---
+++ MvcRichard/Factory/LoadKeysLittleDropsOfMercy.cs
@@ -0,0 +1,36 @@
+﻿using MvcRichard.Models;
+using System.Collections.Generic;
+
+namespace MvcRichard.Factory
+{
+    internal class LoadKeysLittleDropsOfMercy
+    {
+        private static LoadKeysLittleDropsOfMercy _instance;
+
+        public static List<BookModel> list = new List<BookModel>();
+
+        // Constructor is 'protected'
+        protected LoadKeysLittleDropsOfMercy()
+        {
+            int counter = 0;
+            //talks
+
+            list.Add(new BookModel(counter++, "Intro"));
+            
+
+
+        }
+
+        public static LoadKeysLittleDropsOfMercy Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new LoadKeysLittleDropsOfMercy();
+            }
+
+            return _instance;
+        }
+    }
+}
