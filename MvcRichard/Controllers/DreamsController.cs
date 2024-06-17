fa47576484270563fcaf7a3890b78f12da76a0ca
richardpfletcher <richardpfletcher@gmail.com>
2024-06-17 08:43:08 -0500

    Created Dreams

---
+++ MvcRichard/Controllers/DreamsController.cs
@@ -0,0 +1,25 @@
+﻿using MvcRichard.Factory;
+using MvcRichard.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MvcRichard.Controllers
+{
+    public class DreamsController : Controller
+    {
+        public ActionResult DreamsMenu()
+        {
+            return View();
+        }
+
+        public ActionResult Dreams()
+        {
+            
+            return View();
+        }
+    }
+
+}
